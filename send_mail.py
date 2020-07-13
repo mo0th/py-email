258f09e4d7b3b794671282dbe85a4ad19380ab20
Soorria Saruva <soorria.ss@gmail.com>
2020-07-13 11:24:37 +1000

    Add prompt for target email

--- send_mail.py
+++ send_mail.py
@@ -6,17 +6,21 @@ import smtplib
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_TARGET = os.environ.get('EMAIL_TARGET')
 
 if not EMAIL_ADDRESS:
-    EMAIL_ADDRESS = input('Enter an email address:\n')
+    EMAIL_ADDRESS = input("Enter the sender's email address:\n")
 
 if not EMAIL_PASSWORD:
     EMAIL_PASSWORD = getpass(f'Enter the password for {EMAIL_ADDRESS}:\n')
 
+if not EMAIL_ADDRESS:
+    EMAIL_TARGET = input("Enter the target email address:\n")
+
 msg = EmailMessage()
 msg['Subject'] = 'Cool subject'
 msg['From'] = EMAIL_ADDRESS
-msg['To'] = 'mo0th@example.com'
+msg['To'] = EMAIL_TARGET
 msg.set_content('Cool content')
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
